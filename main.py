aa9cdbe511e9685896ac3668249642418b676ab2
judahtc <jaycea247@gmail.com>
2025-07-30 07:20:23 +0200

    debug

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from api import patients, visitors, admins, hospital, security
+# from api import patients, visitors, admins, hospital, security
 
 from fastapi import FastAPI, HTTPException
 import httpx
@@ -25,35 +25,9 @@ def read_root():
     return {"Hey": "I am working"}
 
 
-@app.get("/{username}")
-async def get_user_gists(username: str):
 
-    url = f"{GITHUB_API_BASE_URL}/users/{username}/gists"
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url)
-        if response.status_code == 200:
-            gists = response.json()
-            # Extract relevant data from each gist
-            return [
-                {
-                    "id": gist["id"],
-                    "description": gist.get("description", "No description"),
-                    "url": gist["html_url"],
-                }
-                for gist in gists
-            ]
-        elif response.status_code == 404:
-            raise HTTPException(status_code=404, detail="User not found")
-        else:
-            raise HTTPException(
-                status_code=response.status_code, detail=response.json())
-    except httpx.RequestError as exc:
-        raise HTTPException(
-            status_code=500, detail=f"Error connecting to GitHub API: {exc}")
-
-app.include_router(security.router)
-app.include_router(hospital.router)
-app.include_router(admins.router)
-app.include_router(patients.router)
-app.include_router(visitors.router)
+# app.include_router(security.router)
+# app.include_router(hospital.router)
+# app.include_router(admins.router)
+# app.include_router(patients.router)
+# app.include_router(visitors.router)
